25f668e74e2632d8c3a676194f7e83bc7cf261d4
Mike <mclancy96@gmail.com>
2025-06-09 15:08:36 -0400

    Project list passing

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+const displayProjectItems = (projects) => {
+  return projects.map(project => (
+    <ProjectItem
+      key={project.id}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}
+    />
+  ))
+}
+
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {displayProjectItems(projects)}
+      </div>
     </div>
   );
 }
